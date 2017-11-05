5441107aee154e139d28a274b35cd65d50690855
Nikola Rados <nikola.rados@s-sm.org>
2017-11-04 18:54:56 -0700

    Message Prompt to user for CIA

--- Client.java
+++ Client.java
@@ -9,6 +9,9 @@ import java.net.UnknownHostException;
 public class Client {
 
     public static void main(String[] args) throws UnknownHostException, IOException {
+        int CIA = getCIA();
+        System.out.println(CIA);
+
         String msg, temp;
 
         Scanner scan = new Scanner(System.in);
@@ -24,8 +27,51 @@ public class Client {
             temp = scan1.nextLine();
             System.out.println(temp);
         }
-        catch(NoSuchElementException) {
+        catch(NoSuchElementException e) {
             System.out.println("--- No message found ---");
         }
+    }
+
+    // prompts user for with security options
+    private static int getCIA() {
+        char selection; // user input
+        int triad = 0;  // security triad selection
+        int val = 0;
+        boolean check = true;
+        Scanner scan = new Scanner(System.in);
+
+        for(int i = 0; i < 3; i++) {
+            check = true;
+            // Confidentiality
+            if(i == 0) {
+                System.out.println("Would you like Confidentiality? (y/n)");
+                val = 4;
+            // Integrity
+            } else if (i == 1) {
+                System.out.println("Would you like Integrity? (y/n)");
+                val = 2;
+            // Authentication
+            } else if (i == 2) {
+                System.out.println("Would you like Authentication? (y/n)");
+                val = 1;
+            } else {
+                //we should never get here
+            }
+
+            // loop to prompt user
+            while(check) {
+                selection = scan.next().charAt(0);
+                if(selection == 'y') {
+                    triad += val;
+                    check = false;
+                } else if (selection == 'n') {
+                    //do nothing
+                    check = false;
+                } else {
+                    System.out.println("Please enter a valid option");
+                }
+            }
+        }
+        return triad;
     }
 }
