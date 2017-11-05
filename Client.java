712353e4d0b52676f5055c8e0f7a05b6ddcdf289
Nikola Rados <nikola.rados@s-sm.org>
2017-11-04 18:15:15 -0700

    Fixes for Client connection error

--- Client.java
+++ Client.java
@@ -1,4 +1,5 @@
 import java.util.Scanner;
+import java.util.NoSuchElementException;
 import java.io.PrintStream;
 import java.io.IOException;
 import java.net.Socket;
@@ -7,19 +8,24 @@ import java.net.UnknownHostException;
 
 public class Client {
 
-  public static void main(String[] args) throws UnknownHostException, IOException {
-    String msg, temp;
+    public static void main(String[] args) throws UnknownHostException, IOException {
+        String msg, temp;
 
-    Scanner scan = new Scanner(System.in);
-    Socket socket = new Socket("127.0.0.1", 7082);
-    Scanner scan1 = new Scanner(socket.getInputStream());
+        Scanner scan = new Scanner(System.in);
+        Socket socket = new Socket("127.0.0.1", 7802);
+        Scanner scan1 = new Scanner(socket.getInputStream());
 
-    System.out.println("Enter a message:\n");
-    msg = scan.nextLine();
-    PrintStream p = new PrintStream(socket.getOutputStream());
-    p.println(msg);
-    temp = scan1.nextLine();
-    System.out.println(temp);
+        System.out.println("Enter a message:\n");
+        msg = scan.nextLine();
+        PrintStream p = new PrintStream(socket.getOutputStream());
+        p.println(msg);
 
-  }
+        try {
+            temp = scan1.nextLine();
+            System.out.println(temp);
+        }
+        catch(NoSuchElementException) {
+            System.out.println("--- No message found ---");
+        }
+    }
 }
